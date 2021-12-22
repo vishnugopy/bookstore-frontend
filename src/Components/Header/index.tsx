20fe31582d5f0826a3bf3b6fa5fe401e8d062e7c
vishnugopy <vishnugoby0010@gmail.com>
2021-12-22 10:52:30 +0100

    header done

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -13,15 +13,20 @@ function Header() {
           src="https://source.unsplash.com/random/400x200"
           sx={{ width: 100, height: 100 }}
         />
-        <div className="ads">ads</div>
+        <div className="ads">Advertisement</div>
         <Button startIcon={<Shopping />}>Shopping Cart</Button>
         <Button>Account Management</Button>
       </section>
       <section className="secondline">
         <span>Gendre</span>
-        <Link to="/">Home</Link>
-        <Link to="/">Home</Link>
-        <Link to="/">Home</Link>
+        <Link to="/">New Title</Link>
+        <Link to="/">What's Popular</Link>
+        <Link to="/">Bargin</Link>
+        <Link to="/">Gifts</Link>
+        <label>
+          <input type="input" />
+          <Button>Search</Button>
+        </label>
       </section>
     </header>
   );
